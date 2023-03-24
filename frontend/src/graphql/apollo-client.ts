b8fb86f4e7122ae5181430f69ed96887e2b74e06
Mustapha Limar <tapham079@gmail.com>
2023-03-24 12:52:38 +0000

    websockets client setup

--- frontend/src/graphql/apollo-client.ts
+++ frontend/src/graphql/apollo-client.ts
@@ -1,11 +1,39 @@
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink, split } from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { createClient } from "graphql-ws";
 
 const httpLink = new HttpLink({
   uri: "http://localhost:4000/graphql",
   credentials: "include",
 });
 
+// Checking for the exisiting of the window object, thus we're in the client not in the server !
+const wsLink =
+  typeof window !== undefined
+    ? new GraphQLWsLink(
+        createClient({
+          url: "ws://localhost:4000/graphql/subscriptions",
+        })
+      )
+    : null;
+
+const link =
+  typeof window !== undefined && wsLink !== null
+    ? split(
+        ({ query }) => {
+          const definition = getMainDefinition(query);
+          return (
+            definition.kind === "OperationDefinition" &&
+            definition.operation === "subscription"
+          );
+        },
+        wsLink,
+        httpLink
+      )
+    : httpLink;
+
 export const client = new ApolloClient({
-  link: httpLink,
+  link,
   cache: new InMemoryCache(),
 });
